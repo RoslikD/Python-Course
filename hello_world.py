78914c86d8b20e0b0f4eb8ae6ec5a3836425ca83
Рослик Дима <roslik-eko@yandex.ru>
2023-01-12 11:25:20 +0300

    тренировка от 12.01 - 2-й коммит

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
 #hello_world.py
-print("Hello Python world!")
+#print("Hello Python world!")
 
 message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crach Course world!"
 print(message)
