7a8988dc4c3d3ba0f05f2bdddb9ded2346826599
Рослик Дима <roslik-eko@yandex.ru>
2023-01-13 00:04:10 +0300

    f-strinf

--- full_name.py
+++ full_name.py
@@ -3,3 +3,6 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f" Hello, {full_name.title()}!"
 print(message)
+
+#format
+full_name = "{} {}".format(first_name, last_name)
