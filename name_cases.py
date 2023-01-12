b39b79dbf8d828d633a51d2bc7310343c6c45069
Рослик Дима <roslik-eko@yandex.ru>
2023-01-13 00:53:51 +0300

    Упражнения к главе 2

---
+++ name_cases.py
@@ -0,0 +1,26 @@
+#2.3
+Dima = "Hello Eric, would you like to learn some Python today?"
+print(Dima)
+
+#2.4
+n = 'Dmitry'
+print(n.title())
+print(n.upper())
+print(n.lower())
+
+#2.5
+ci = "'Без труда не вытянешь и рыбку из пруда' - Народная мудрость"
+print(ci)
+
+#2.6
+famous_person = 'Народная мудрость'
+message = 'Без труда не вытянешь и рыбку из пруда'
+print(f"'{message}' - {famous_person}")
+
+#2.7
+person = ' Alexey   '
+print(person.rstrip())
+print(person.lstrip())
+print(person.strip())
+
+
